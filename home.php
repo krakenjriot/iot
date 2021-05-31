e662ef602553b291cfba8ef7a021e1d6fd49cafc
rollly <marclunaris@gmail.com>
2021-06-01 00:49:40 +0300

    t

--- home.php
+++ home.php
@@ -721,28 +721,28 @@
       <!-- Sidebar -->
       <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
         <!-- Sidebar - Brand -->
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
-          <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
-          </div>
-          <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+		</br>
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+          <!--<div class="sidebar-brand-icon">            
+			<i class="far fa-user-alien"></i>
+          </div>-->
+          <div class="sidebar-brand-text mx-3">PORTTY Command Center</div>
         </a>
+		</br>
         <!-- Divider -->
         <hr class="sidebar-divider my-0">
-        <!-- Nav Item - Dashboard -->
-        <li class="nav-item active">
-          <a class="nav-link" href="index.php">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span></a>
-        </li>
-        <!-- Divider -->
-        <hr class="sidebar-divider">
+      
+        <!-- Divider
+        <hr class="sidebar-divider"> -->
         <!-- Heading -->
         <div class="sidebar-heading">
           Interface
         </div>
+		
+		
         <!-- Nav Item - Pages Collapse Menu -->
-        <li class="nav-item">
+        <!--
+		<li class="nav-item">
           <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
             aria-expanded="true" aria-controls="collapseTwo">
           <i class="fas fa-fw fa-cog"></i>
@@ -756,6 +756,9 @@
             </div>
           </div>
         </li>
+		-->
+		
+		
         <!-- Nav Item - Utilities Collapse Menu -->
         <li class="nav-item">
           <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
@@ -767,10 +770,10 @@
             data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
               <h6 class="collapse-header">Custom Utilities:</h6>
-              <a class="collapse-item" href="utilities-color.php">Colors</a>
-              <a class="collapse-item" href="utilities-border.php">Borders</a>
-              <a class="collapse-item" href="utilities-animation.php">Animations</a>
-              <a class="collapse-item" href="utilities-other.php">Other</a>
+              <a class="collapse-item" href="#">DHT</a>
+              <a class="collapse-item" href="#">PH</a>
+              <a class="collapse-item" href="#p">Levels</a>
+              <a class="collapse-item" href="#">Other</a>
             </div>
           </div>
         </li>
@@ -781,7 +784,8 @@
           Addons
         </div>
         <!-- Nav Item - Pages Collapse Menu -->
-        <li class="nav-item">
+        <!--
+		<li class="nav-item">
           <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
             aria-expanded="true" aria-controls="collapsePages">
           <i class="fas fa-fw fa-folder"></i>
@@ -800,6 +804,7 @@
             </div>
           </div>
         </li>
+		-->
         <!-- Nav Item - Charts -->
         <li class="nav-item">
           <a class="nav-link" href="#mark-server">
@@ -818,18 +823,8 @@
           <i class="fas fa-fw fa-chart-area"></i>
           <span>Pins</span></a>
         </li>
-        <!-- Nav Item - Charts -->
-        <li class="nav-item">
-          <a class="nav-link" href="charts.php">
-          <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span></a>
-        </li>
-        <!-- Nav Item - Tables -->
-        <li class="nav-item">
-          <a class="nav-link" href="tables.php">
-          <i class="fas fa-fw fa-table"></i>
-          <span>Tables</span></a>
-        </li>
+  
+     
         <!-- Divider -->
         <hr class="sidebar-divider d-none d-md-block">
         <!-- Sidebar Toggler (Sidebar) -->
